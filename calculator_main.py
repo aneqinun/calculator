658f87ebde35bd18ad23896bbd8856d2fa4b62c4
Alex Quinn <alex.quinn@featurespace.co.uk>
2020-05-29 10:07:57 +0100

    [CALC1-0002] Add basic window and input for GUI

--- calculator_main.py
+++ calculator_main.py
@@ -42,3 +42,33 @@ def clear():
     global heldExpression
     heldExpression = ""
     equation.set("")
+
+# Driver code
+if __name__ == "__main__":
+    # create a GUI window
+    gui = Tk()
+
+    # set the background colour of GUI window
+    gui.configure(background="gainsboro")
+
+    # set the title of GUI window
+    gui.title("Calculator")
+
+    # set the configuration of GUI window
+    gui.geometry("490x270")
+
+    # StringVar() is the variable class
+    # we create an instance of this class
+    equation = StringVar()
+
+    # create the text entry box for
+    # showing the heldExpression
+    heldExpression_field = Entry(gui, textvariable=equation)
+
+    # grid method is used for placing
+    # the widgets at respective positions
+    # in table like structure .
+    heldExpression_field.grid(columnspan=8, ipadx=153)
+
+    # start the GUI
+    gui.mainloop()
